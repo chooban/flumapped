6aba83a00dbab36bc8747ce430f80f27b6eefcaf
Ross Hendry <rhendry@googlemail.com>
2018-01-15 12:08:07 +0000

    Now with a legend and colours

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 Eventually might be a choropleth of volunteer numbers for the flu study, if such data can be displayed.
 
 Postcode info nabbed from [here](https://github.com/roblascelles/uk-postcode-map).
+
+# Running It
+
+Run `python -m SimpleHTTPServer` in the cloned directory and point a browser at http://localhost:8000
--- flumap.js
+++ flumap.js
@@ -1,4 +1,4 @@
-var width = 960,
+var width = 480,
   height = 1160;
 
 var scottishPostcodes = [
@@ -20,34 +20,139 @@ var scottishPostcodes = [
   'ZE'
 ];
 
-var projection = d3.geo.albers()
+var formatPercent = d3.format(".0%");
+var formatNumber = d3.format(".0f");
+
+var projection = d3.geoAlbers()
   .center([0, 55.4])
   .rotate([4.4, 0])
   .parallels([50, 60])
   .scale(1200 * 5)
   .translate([width / 2, height / 2]);
 
-var path = d3.geo.path()
+var path = d3.geoPath()
   .projection(projection);
 
-var svg = d3.select("body").append("svg")
-  .attr("width", width)
-  .attr("height", height);
+var fillColor = d3.scaleThreshold()
+  .domain([0.01, 0.02, 0.05, 0.1, 0.25])
+  .range(["#f2f0f7", "#dadaeb", "#bcbddc", "#9e9ac8", "#756bb1", "#54278f"]);
 
-d3.json("uk-postcode-area.json", function(err, uk) {
-  if (!err) {
-    var areas = uk.objects['uk-postcode-area'].geometries.filter(function(a) {
-      return scottishPostcodes.includes(a.id);
+var x = d3.scaleLinear()
+  .domain([0, 1])
+  .range([0, 240]);
+
+var xAxis = d3.axisBottom(x)
+  .tickSize(13)
+  .tickValues(fillColor.domain())
+  .tickFormat(function(d) {
+    return d === 0.5 ? formatPercent(d) : formatNumber(100 * d);
+  });
+
+function centre(d) {
+  return "translate(" + path.centroid(d) + ")";
+}
+
+d3.queue()
+  .defer(d3.json, "uk-postcode-area.json")
+  .defer(d3.tsv, "mock-data.tsv")
+  .await(transform)
+
+function transform(err, uk, data) {
+  var countsByPostcode = _.chain(data)
+    .countBy(function(e) {
+      return e.postcode.match(/^([A-Z]+)/)[1];
+    })
+    .mapValues(function(e) {
+      return e / data.length;
+    })
+    .value();
+
+  var areas = uk.objects['uk-postcode-area'].geometries.filter(function(a) {
+    return scottishPostcodes.includes(a.id);
+  });
+
+  uk.objects['sco-postcode-area'] = {
+    type: 'GeometryCollection',
+    geometries: areas
+  };
+
+  draw(uk, countsByPostcode);
+}
+
+function draw(uk, counts) {
+  var svg = d3.select("body").append("svg")
+    .attr("width", width)
+    .attr("height", height);
+
+  svg.selectAll(".subunit")
+    .data(topojson.feature(uk, uk.objects['sco-postcode-area']).features)
+    .enter()
+    .append("path")
+      .attr("class", "postcode_area")
+      .attr("d", path)
+      .style("fill", function(d) {
+        return fillColor(counts[d.id]);
+      })
+      .append("svg:title")
+        .attr("transform", centre)
+        .attr("dy", ".35em")
+        .text(function(d) { return d.id; });
+
+  svg.append("path")
+    .datum(topojson.mesh(uk, uk.objects['sco-postcode-area'], function(a, b) {
+      return a !== b;
+    }))
+    .attr("class", "mesh")
+    .attr("d", path);
+
+  var labelData = fillColor.range().map(function(c) {
+      var d = fillColor.invertExtent(c);
+      if (d[0] == null) { d[0] = x.domain()[0]; }
+      if (d[1] == null) { d[1] = x.domain()[1]; }
+      return d;
     });
 
-    svg.selectAll(".subunit")
-      .data(topojson.feature(uk, {
-        type: 'GeometryCollection',
-        geometries: areas
-      }).features)
-      .enter()
-        .append("path")
-          .attr("class", "feature")
-          .attr("d", path);
-  }
-});
+  var axisGroup = svg.append("g")
+      .attr("class", "axis")
+      .attr("transform", "translate(0, 700)");
+
+  axisGroup.append("text")
+    .attr("class", "title")
+    .attr("fill", "#000")
+    .attr("font-size", "12px")
+    .attr("font-weight", "bold")
+    .attr("y", "1em")
+    .text("Percentage of volunteers from each postcode area")
+
+  var keyGroup = axisGroup.append("g")
+    .attr("transform", "translate(0, 20)");
+
+  keyGroup.selectAll("rect")
+    .data(labelData)
+    .enter()
+      .insert("rect")
+        .attr("class", "tick")
+        .attr("height", 8)
+        .attr("x", function(d, i) {
+          return i * 75;
+        })
+        .attr("width", 75)
+        .attr("fill", function(d) {
+          return fillColor(d[0]);
+        });
+
+    keyGroup.selectAll("text.ticklabel")
+      .data(labelData)
+      .enter().append("text")
+        .attr("class", "ticklabel")
+        .attr("x", "0.35em")
+        .attr("y", "1.5em")
+        .attr("font-size", "12px")
+        .attr("text-anchor", "middle")
+        .attr("dx", function(d, i) {
+          return (i * 75) + 33;
+        })
+        .text(function(d) {
+          return d[0] + " - " + d[1];
+        });
+}
